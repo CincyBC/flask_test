5a73db602a8f2122c99bfba92c6b470b07655300
cincybc <Cleo12#$>
2022-08-07 15:15:13 -0400

    Fixing gunicorn

--- app.py
+++ app.py
@@ -11,45 +11,45 @@ app = Flask(__name__)
 UPLOAD_FOLDER = 'output/'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-# def allowed_file(filename):
-#     return '.' in filename and \
-#            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/', methods=['GET'])
 def index():
     return "<h1>Welcome to the UWW Prediction App</h1>", 200 
 
 
-# @app.route('/upload', methods=['GET', 'POST'])
-# def upload():
-#     if request.method == 'POST':
-#         file = request.files['file']
-#         if file and allowed_file(file.filename):
-#             filename = secure_filename(file.filename)
-#             new_filename = f'{filename.split(".")[0]}_{str(datetime.now())}.csv'
-#             save_location = os.path.join('input', new_filename)
-#             file.save(save_location)
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            new_filename = f'{filename.split(".")[0]}_{str(datetime.now())}.csv'
+            save_location = os.path.join('input', new_filename)
+            file.save(save_location)
 
-#             output_file = process_csv(save_location)
-#             #return send_from_directory('output', output_file)
-#             return redirect(url_for('download'))
+            output_file = process_csv(save_location)
+            #return send_from_directory('output', output_file)
+            return redirect(url_for('download'))
 
-#     return render_template('upload.html')
+    return render_template('upload.html')
 
-# @app.route('/download')
-# def download():
-#     return render_template('download.html', files=os.listdir('output'))
+@app.route('/download')
+def download():
+    return render_template('download.html', files=os.listdir('output'))
 
-# @app.route('/download/<filename>')
-# def download_file(filename):
-#     return send_from_directory('output', filename)
+@app.route('/download/<filename>')
+def download_file(filename):
+    return send_from_directory('output', filename)
 
-# Invalid URL
-# @app.errorhandler(404)
-# def page_not_found(e):
-# 	return render_template("404.html"), 404
+Invalid URL
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template("404.html"), 404
 
-# # Internal Server Error
-# @app.errorhandler(500)
-# def page_not_found(e):
-# 	return render_template("500.html"), 500
+# Internal Server Error
+@app.errorhandler(500)
+def page_not_found(e):
+	return render_template("500.html"), 500
