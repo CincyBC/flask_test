5e11bd054994d17119589db32fdb78ee19157268
cincybc <Cleo12#$>
2022-08-07 15:00:29 -0400

    Added comments

--- app.py
+++ app.py
@@ -19,21 +19,21 @@ def index():
     return "<h1>Welcome to the UWW Prediciton App</h1>", 200 
 
 
-@app.route('/upload', methods=['GET', 'POST'])
-def upload():
-    if request.method == 'POST':
-        file = request.files['file']
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            new_filename = f'{filename.split(".")[0]}_{str(datetime.now())}.csv'
-            save_location = os.path.join('input', new_filename)
-            file.save(save_location)
+# @app.route('/upload', methods=['GET', 'POST'])
+# def upload():
+#     if request.method == 'POST':
+#         file = request.files['file']
+#         if file and allowed_file(file.filename):
+#             filename = secure_filename(file.filename)
+#             new_filename = f'{filename.split(".")[0]}_{str(datetime.now())}.csv'
+#             save_location = os.path.join('input', new_filename)
+#             file.save(save_location)
 
-            output_file = process_csv(save_location)
-            #return send_from_directory('output', output_file)
-            return redirect(url_for('download'))
+#             output_file = process_csv(save_location)
+#             #return send_from_directory('output', output_file)
+#             return redirect(url_for('download'))
 
-    return render_template('upload.html')
+#     return render_template('upload.html')
 
 # @app.route('/download')
 # def download():
