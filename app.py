8d6df00939c7bbc9d3fe4cf29e2fbd4dae9444e1
cincybc <Cleo12#$>
2022-08-07 22:19:24 -0400

    added ml model

--- app.py
+++ app.py
@@ -3,13 +3,11 @@ import os
 from flask import Flask, request, render_template, redirect, url_for, send_from_directory
 from werkzeug.utils import secure_filename
 from datetime import datetime
-from process import process_csv
+from run import run_script
 
 app = Flask(__name__)
 
 ALLOWED_EXTENSIONS = set(['csv'])
-UPLOAD_FOLDER = 'output/'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -17,7 +15,7 @@ def allowed_file(filename):
 
 @app.route('/', methods=['GET'])
 def index():
-    return "<h1>Welcome to the UWW Prediction App</h1>", 200 
+    return "<h1>DataKind</h1><br><h2>Partning with United Way</h2>", 200 
 
 
 @app.route('/upload', methods=['GET', 'POST'])
@@ -26,23 +24,21 @@ def upload():
         file = request.files['file']
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            new_filename = f'{filename.split(".")[0]}_{str(datetime.now())}.csv'
-            save_location = os.path.join('input', new_filename)
+            save_location = os.path.join('input', filename)
             file.save(save_location)
 
-            output_file = process_csv(save_location)
-            #return send_from_directory('output', output_file)
+            output_file = run_script(save_location)
             return redirect(url_for('download'))
 
     return render_template('upload.html')
 
 @app.route('/download')
 def download():
-    return render_template('download.html', files=os.listdir('output'))
+    return render_template('download.html', files=os.listdir('run/model_scoring'))
 
 @app.route('/download/<filename>')
 def download_file(filename):
-    return send_from_directory('output', filename)
+    return send_from_directory('run/model_scoring', filename)
 
 # Invalid URL
 @app.errorhandler(404)
